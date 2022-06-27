ce103f1dcc251b8ecb70bbc04a8039530f920079
ALLaTk <regicter1810@mail.ru>
2022-06-27 19:07:12 +0300

    feat: add interfaces for data exchange with News API

--- migration-to-ts/components/controller/appLoader.js
+++ migration-to-ts/components/controller/appLoader.js
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: '4c67d8c9aef14c1b8ced7747e25bd186',
         });
     }
 }
