a586974c5a60dad08ec9b26353470ca140572b73
ALLaTk <regicter1810@mail.ru>
2022-07-04 15:18:17 +0300

    refactor: change apiKey

--- migration-to-ts/components/controller/appLoader.ts
+++ migration-to-ts/components/controller/appLoader.ts
@@ -3,9 +3,8 @@ import { Loader } from './loader';
 class AppLoader extends Loader {
   constructor() {
     super('https://nodenews.herokuapp.com/', {
-      apiKey: '4c67d8c9aef14c1b8ced7747e25bd186',
+      apiKey: '922bb8b4ac4345b6851bffc1ea37de62',
       // '4c67d8c9aef14c1b8ced7747e25bd186'
-      // 922bb8b4ac4345b6851bffc1ea37de62
     });
   }
 }
