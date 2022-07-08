bc3584d3a8467d71d6b52364069e3713b5db0314
ALLaTk <regicter1810@mail.ru>
2022-07-09 01:04:33 +0300

    feat: add the Sort class, sort.cscc and Sort.ts files

---
+++ online-store/components/views/main/sort/Sort.ts
@@ -0,0 +1,25 @@
+import './sort.scss';
+import Control from '../../Control';
+
+class Sort extends Control {
+  constructor(parent: HTMLElement) {
+    super(parent, 'div', 'sort');
+  }
+
+  render() {
+    const html = `
+    <form action="" method="post">
+      <p><select class="select">
+        <option selected value="Price(Lowest)">Price (Lowest)</option>
+        <option value="Price(Highest)">Price (Highest)</option>
+        <option value="Name(A-Z)">Name (A-Z)</option>
+        <option value="Name(Z-A)">Name (Z-A)</option>
+     </select></p>
+  </form>`;
+
+    this.element.innerHTML = html;
+  }
+}
+
+const sort = new Sort(document.body);
+export default sort;
--- online-store/index.ts
+++ online-store/index.ts
@@ -1,4 +1,6 @@
 import './global.scss';
 import header from './components/views/header/Header';
+import sort from './components/views/main/sort/Sort';
 
 header.render(7);
+sort.render();
