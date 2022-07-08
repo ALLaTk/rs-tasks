b6f8277fe61fef0c2b573c4229475efeda4036fd
ALLaTk <regicter1810@mail.ru>
2022-07-09 00:46:46 +0300

    feat: add Control class

---
+++ online-store/components/views/Control.ts
@@ -0,0 +1,20 @@
+class Control {
+  element: HTMLElement;
+  mainContent: HTMLElement
+  node!: { remove: () => void };
+
+  constructor(parent: HTMLElement, tag: string, name: string) {
+    this.element = document.createElement(tag);
+    this.element.className = name;
+    parent.append(this.element);
+    this.mainContent = document.createElement('main')
+    this.mainContent.className = 'main'
+  }
+
+  destroy(): void {
+    this.node.remove();
+  }
+  
+}
+
+export default Control;
