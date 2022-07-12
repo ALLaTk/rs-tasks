d4d9803ece41393e05a46d280f4c008996fe8e11
ALLaTk <regicter1810@mail.ru>
2022-07-12 21:49:02 +0300

    refactor: refactor index.ts, index.html and global.scss files

--- online-store/index.ts
+++ online-store/index.ts
@@ -1,14 +1,7 @@
 import './global.scss';
-import header from './components/views/header/Header';
-import sort from './components/views/main/sort/Sort';
-import cards from './components/views/main/cards/Cards';
-import filter from './components/views/main/filter/Filter';
+import App from './components/controller/App';
 
-header.render(7);
-cards.render();
-sort.renderSort();
-sort.renderSearch();
-filter.changeCompany();
-filter.changePower();
-filter.changeColor();
-filter.changePopular();
+const app = new App()
+app.start()
+
+
